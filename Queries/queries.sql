605dfddf02557859cfe7f3a584e16811c41ae558
GrisellTavitas <griselltavitasm@gmail.com>
2021-11-07 19:25:20 -0500

    query tables

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -43,3 +43,202 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d 
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retiremennt_ info and dept:emp tables
+SELECT retirement_info.emp_no, 
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d 
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retiremennt_ info and dept:emp tables
+SELECT retirement_info.emp_no, 
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO ret_bydept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM ret_bydept;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Employee information
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT 	dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+		INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+
+-- Department Retirees			
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Retiring info for sales deptment
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+d.dept_name
+INTO salesdept_info
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+-- Mentoring Program info
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+d.dept_name
+INTO mentoring_program
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- Deliverable 1
+-- a) List of retirement employees & correspondant titles
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+ce.to_date, 
+ti.title,
+ti.from_date
+-- INTO retirement_titles
+FROM current_emp AS ce
+LEFT JOIN titles AS ti
+ON (ce.emp_no = ti.emp_no)
+INNER JOIN employees AS e
+ON (ce.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+-- b) Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title, 
+rt.from_date,
+rt.to_date
+INTO retirement_titles_unique
+FROM retirement_titles AS rt
+ORDER BY emp_no, from_date DESC;
+
+**GUIDED**
+-- Deliverable 1
+-- a) List of retirement employees & correspondant titles
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date 
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- b) Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title 
+INTO unique_titles
+FROM retirement_titles 
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- c)  Number of titles
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
